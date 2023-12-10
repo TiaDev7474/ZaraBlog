967f8b76ff6d631b140256ab7bfd0a81f2b8203f
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-12-10 16:02:13 +0300

    refactor: made some refacotring inside auth logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '2h' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   providers: [AuthService],
